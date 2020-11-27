ef97dd1abcd481be9e8fe1d2e79417086bee67dd
Federico Pigato <federico.pigato2001@gmail.com>
2020-11-27 23:16:30 +0100

    per-server settings

--- commands/settings.js
+++ commands/settings.js
@@ -2,9 +2,9 @@ module.exports = {
     name: "setting",
     description: "Aiuto con i comandi",
     execute(message, args, db) {
-        console.log(db.get(message.guild.name));
-        if(args.lenght == 0) {
+        console.log(JSON.stringify(db.get(message.guild.name)));
+        // if(args.lenght == 0) {
             message.channel.send(JSON.stringify(db.get(message.guild.name)));
-        }
+        // }
     }
 }
