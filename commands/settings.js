27b1d4faf6d263cb16579c91bdd4f65ceed1798e
Federico Pigato <federico.pigato2001@gmail.com>
2020-11-29 14:12:40 +0100

    changing with embed messages

--- commands/settings.js
+++ commands/settings.js
@@ -4,27 +4,15 @@ module.exports = {
     name: "setting",
     description: "Change settings",
     execute(message, args, db) {
-        let stringDb = JSON.stringify(db.get(message.guild.name));
-        stringDb = stringDb.replace("{", "");
-        stringDb = stringDb.replace("}", "");
-        // stringDb = stringDb.replace(",", "\n");
-        stringDb = stringDb.replace(/['"]+/g, "");
-        let res = stringDb.split(/[:,]+/g);
-
         if(args.length == 0) {
             let embed = new Discord.MessageEmbed()
-            .setColor("#700d75")
+            .setColor("#1d5791")
             .setDescription("Settings");
             for(let key in db.get(message.guild.name)) {
                 embed.addField(key, db.get(message.guild.name + "." + key), true);
             }
+
             message.channel.send(embed);
-        } else if(args.length == 1){
-            for(let key in db.get(message.guild.name)) {
-                if(args[0].toLowerCase() == key) {
-                    message.channel.send(db.get(message.guild.name + "." + key));
-                }
-            }
         } else if(args.length == 2) {
             for(let key in db.get(message.guild.name)) {
                 if(args[0].toLowerCase() == key) {
