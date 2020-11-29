d811a735e1703cb2da3682d5049267583cad5c7e
Federico Pigato <federico.pigato2001@gmail.com>
2020-11-29 21:44:21 +0100

    better help command

--- commands/help.js
+++ commands/help.js
@@ -11,6 +11,7 @@ module.exports = {
       .setDescription("Command list")
       .setFooter("by Pyguz.#0456", "https://cdn.discordapp.com/avatars/484387014725206016/4113368f74bd7056a02b20b03b2995a3.png");
       for(let key in Array.from(commands.keys())) {
+        console.log(key, commands.get(key).whatDo);
         embed.addField(key, commands.get(key).whatDo, true);
       }
 
