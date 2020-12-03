7e6cdcf577e3c75a8ea6667e8b558b1bd1caf0ab
Federico Pigato <federico.pigato2001@gmail.com>
2020-12-03 23:54:48 +0100

    gildUpdate

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ client.on("guildCreate", guild => {
 });
 
 client.on("guildUpdate", (oldGuild, newGuild) => {
-    DB.set(newGuild.name, DB.table(oldGuild.name));
-    DB.delete(oldGuild);
+    DB.set(newGuild.name, DB.get(oldGuild.name));
+    DB.delete(oldGuild.name);
 });
 
 client.on("message", message => {
@@ -46,7 +46,7 @@ client.on("message", message => {
     }
     if(message.author.bot) return;
 
-    console.log(DB.table(SERVER.name));
+    console.log(DB.get(SERVER.name));
     let args = message.content.slice(PREFIX.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
